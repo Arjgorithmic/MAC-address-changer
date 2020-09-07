7c54ad7ba2e0e28b16feadeca207afc11baf7153
ARJ-cyber <arjunpdinesh123@gmail.com>
2020-09-07 10:46:50 +0530

    Change your mac address

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,6 @@
 import subprocess
 
 
-subprocess.call("ifconfig eth0 down" ,shell=True)
-subprocess.call("ifconfig eth0  hw ether 39:93:B7:46:36:A3 , shell=True)
-subprocess.call("ifconfig eth0 up" , shell=True)
+subprocess.call("ifconfig wlan0 down" ,shell=True)
+subprocess.call("ifconfig wlan0 hw ether 39:93:B7:46:36:A3 , shell=True)
+subprocess.call("ifconfig wlan0 up" , shell=True)
